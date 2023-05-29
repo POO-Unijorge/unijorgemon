bc335f0c2f6dc0ea30709f6b36eae69321f5f551
Luccamvmrey <luccamvmachadorey@gmail.com>
2023-05-29 20:58:33 -0300

    Criada classe abstrata animal.

---
+++ src/org/unijorge/core/Animal.java
@@ -0,0 +1,59 @@
+package org.unijorge.core;
+
+public abstract class Animal {
+    //idade, sexo, nome cientifico, condicao, peso, tamanho
+    private int idade;
+    private double peso;
+    private double tamanho;
+    private String sexo;
+    private String nomeCientifico;
+    private String condicao;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNomeCientifico() {
+        return nomeCientifico;
+    }
+
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    public String getCondicao() {
+        return condicao;
+    }
+
+    public void setCondicao(String condicao) {
+        this.condicao = condicao;
+    }
+}
