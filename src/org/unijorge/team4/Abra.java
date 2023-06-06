ed4e04b9d8271b7cb99606528aec8350bc5e9465
renansbernardo <renann005@gmail.com>
2023-06-06 14:53:56 -0300

    Add um novo pokemon no team4

---
+++ src/org/unijorge/team4/Abra.java
@@ -0,0 +1,13 @@
+package org.unijorge.team4;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.Sexo;
+
+public class Abra extends Pokemon {
+
+    public Abra(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress,
+            int hp, int atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
