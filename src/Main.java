673989a789ff89d22d799da1a239ad4c7a0664d5
Vinicius  de Souza Rezende <06812485560@academico.fja.edu.br>
2023-06-12 20:53:48 -0300

    Alterando o tipo de ataque da Magikarpa

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,6 @@ public class Main {
         Eevee eevee = new Eevee(10, 20.0, 12.5, Sexo.F, "Eevee", 0, 0, 10, 5, 4, 9);
         System.out.println(eevee.toString());
 
-        Magikarp magikarp = new Magikarp(50, 1, 1, Sexo.F, "Magikarpa", 0, 0, 1, new Ataque(1, "Swift Swim", "Elétrico", 1), 0, 0); 
+        Magikarp magikarp = new Magikarp(50, 1, 1, Sexo.F, "Magikarpa", 0, 0, 1, new Ataque(1, "Swift Swim", "Agua", 1), 0, 0); 
     }
 }
