526d5acbc7ebd190fbd6216392d8409f9474f155
Hauntedghostjr <129462880+Hauntedghostjr@users.noreply.github.com>
2023-06-12 15:17:56 -0300

    Add Froakie.java

---
+++ src/org/unijorge/team5/Froakie.java
@@ -0,0 +1,8 @@
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.Sexo;
+ public class Froakie extends Pokemon {
+
+  public Froakie(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, int atk, int def, int spd){
+   super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+ }    
+}
