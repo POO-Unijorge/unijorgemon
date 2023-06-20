f0809697b2193ff24586e64ff57abf6863e8e8ed
natmichel <nmichel582@gmail.com>
2023-06-19 21:13:48 -0300

    alt de tipo do atk em Eevee

--- src/org/unijorge/team6/Eevee.java
+++ src/org/unijorge/team6/Eevee.java
@@ -1,11 +1,11 @@
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
-import org.unijorge.core.Sexo;
+import org.unijorge.core.enum.Sexo;
 
 public class Eevee extends Pokemon{
 
     public Eevee(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
-            int levelProgress, int hp, int atk, Ataque def, int spd) {
+            int levelProgress, int hp, Ataque atk, int def, int spd) {
         super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
         //TODO Auto-generated constructor stub
 
