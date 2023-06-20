92ced236537211659fab94c0d9292eab5731aef5
Douglas Reis <102832693+DouglazReis@users.noreply.github.com>
2023-06-19 21:05:44 -0300

    Update and rename Bewear.java to Porygon.java

--- src/org/unijorge/Team 7/Porygon.java
+++ src/org/unijorge/Team 7/Porygon.java
@@ -3,9 +3,9 @@ import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
 import org.unijorge.core.Sexo;
 
-public class Bewear extends Pokemon{
+public class Porygon extends Pokemon{
 
-	public Bewear (int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+	public Porygon (int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
 			int levelProgress, int hp, Ataque atk, int def, int spd) {
 		super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
 	}
