20acbc389a3ceb4c3c72983c02447c9713cbc6b5
natmichel <nmichel582@gmail.com>
2023-06-19 21:18:11 -0300

    alt eevve

--- src/org/unijorge/team6/Eevee.java
+++ src/org/unijorge/team6/Eevee.java
@@ -1,8 +1,9 @@
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
 import org.unijorge.core.enum.Sexo;
+import org.unijorge.core.intefaces.Normal;
 
-public class Eevee extends Pokemon{
+public class Eevee extends Pokemon implements Normal{
 
     public Eevee(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
             int levelProgress, int hp, Ataque atk, int def, int spd) {
@@ -10,5 +11,9 @@ public class Eevee extends Pokemon{
         //TODO Auto-generated constructor stub
 
     }
-    
+    /*
+     * public static void main(String[] args){
+     *  Eevee eevee = new Eevee(10,10,10,Sexo.F,"blablabla", 1, 0, 35, new Ataque(80, "Tackle", 100));
+     * }
+     */
 }
