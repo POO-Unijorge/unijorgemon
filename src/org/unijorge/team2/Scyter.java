7903f8a32e27d3f34ec3f8f542f4e5fab01575e4
diegopass0s <dyanshythan@hotmail.com>
2023-06-20 23:32:51 -0300

    fix imports; interface inseto cht

--- src/org/unijorge/team2/Scyter.java
+++ src/org/unijorge/team2/Scyter.java
@@ -1,15 +1,30 @@
-public class Scyther extends Animal implements Inseto{
+package org.unijorge.team2;
+
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.enums.Sexo;
+import org.unijorge.core.interfaces.Inseto;
+
+public class Scyter extends Pokemon implements Inseto{
 
 
-    public int setHp(int hp){
+
+
+    public Scyter(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+            int levelProgress, int hp, Ataque atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+        //TODO Auto-generated constructor stub
+    }
+
+    public void setHp(int hp){
         if(getHp()<50){
             if(getSpd()+10<95){
                 setSpd(getSpd()+10);
             }
-            if(getAtk()+10<95){
-                setAtk(getAtk()+10);
+            if((getAtk().getDano()+10)<95){
+                getAtk().setDano(getAtk().getDano()+10);
             }
-            setHp(getHp()+4;)
+            setHp(getHp()+4);
         }
     }
 
