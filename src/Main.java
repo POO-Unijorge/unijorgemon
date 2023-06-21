917e72687982e5d5881c9be19aa665351d9f8efc
diegopass0s <dyanshythan@hotmail.com>
2023-06-20 23:12:50 -0300

    renomeado diretorio

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,12 @@
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
-import org.unijorge.core.Sexo;
 import org.unijorge.team3.Magikarp;
 
 public class Main {
     public static void main(String[] args) {
         //codigo zuretado, não executa, blz? 
         //é só ideia!
-        Eevee p1 = new Eevee(10, 20.0, 12.5, Sexo.F, "Eevee", 0, 0, 10, new Ataque(10, "Tackle", "Normal", 100), 4, 9);
+        System.out.println("ola");
 
-        arena = new Arena (p1, p2, new calculos())
-        arena.batalha()
-        arena.turnos();
-        arena.resultado()
-
-        System.out.println(eevee.toString());
-
-        Magikarp magikarp = new Magikarp(50, 1, 1, Sexo.F, "Magikarpa", 0, 0, 1, new Ataque(1, "Swift Swim", "Agua", 1), 0, 0); 
     }
 }
