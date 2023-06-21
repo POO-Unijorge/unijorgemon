f0fb561e06805eab229817e49d52014d88fbaca7
diegopass0s <dyanshythan@hotmail.com>
2023-06-20 23:45:29 -0300

    fix construtor

--- src/org/unijorge/team5/Froakie.java
+++ src/org/unijorge/team5/Froakie.java
@@ -4,7 +4,7 @@ import org.unijorge.core.enums.Sexo;
  public class Froakie extends Pokemon {
 
   
-  public Froakie(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, int atk, int def, int spd){
-   super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+  public Froakie(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, Ataque atk, int def, int spd){
+    super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
  }    
 }
