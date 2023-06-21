fa241e8b4915a5e410df7f9ce34d654935711948
diegopass0s <dyanshythan@hotmail.com>
2023-06-20 23:14:17 -0300

    renomeado main inteno conflito

---
+++ src/org/unijorge/team7/Porygon.java
@@ -0,0 +1,13 @@
+package org.unijorge.team7;
+import org.unijorge.core.intefaces;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.Sexo;
+
+public class Porygon extends Pokemon implements Normal{
+
+	public Porygon (int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+			int levelProgress, int hp, Ataque atk, int def, int spd) {
+		super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+	}
+}
