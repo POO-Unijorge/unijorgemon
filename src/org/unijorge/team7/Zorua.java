7cfa7caf86374c4f37ded1be5d1fa6a9f6e949dd
Michel <michel.geronimo@hotmail.com>
2023-06-23 16:52:43 -0300

    add +1 pokemon e ajustes

---
+++ src/org/unijorge/team7/Zorua.java
@@ -0,0 +1,12 @@
+package org.unijorge.team7;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.Sexo;
+public class Zorua extends Pokemon{
+
+    public Zorua(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, Ataque atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+    }
+
+
+}
