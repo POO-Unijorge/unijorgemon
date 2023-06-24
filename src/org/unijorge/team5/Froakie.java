83cfeb1d34efbcee1be618f018a12fd2bce880b5
Hauntedghostjr <129462880+Hauntedghostjr@users.noreply.github.com>
2023-06-24 17:23:27 -0300

    Update Froakie.java
    Fiz pequenas mudanças no codigo, como colocar package no time 5  e implmentalo no tipo agua

--- src/org/unijorge/team5/Froakie.java
+++ src/org/unijorge/team5/Froakie.java
@@ -1,8 +1,10 @@
+package org.unijorge.team5;
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
 import org.unijorge.core.enums.Sexo;
+import org.unijorge.core.interfaces.Agua;
 
- public class Froakie extends Pokemon {
+ public class Froakie extends Pokemon implements Agua{
 
   
   public Froakie(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, Ataque atk, int def, int spd){
