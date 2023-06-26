8763cf734c0f46c4a752a16488ea5d63c108796d
Hauntedghostjr <129462880+Hauntedghostjr@users.noreply.github.com>
2023-06-26 20:02:34 -0300

    Update Bubbles.java

--- src/org/unijorge/team5/Bubbles.java
+++ src/org/unijorge/team5/Bubbles.java
@@ -1,41 +1,21 @@
 package org.unijorge.team5;
 import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.enums.Sexo;
 import org.unijorge.core.interfaces.Agua;
 
-public class Bubbles extends Ataque implements Agua {
-    public Bubbles(int dano, String nome, String tipo, double pBaseAtaque) {
-        super(dano, nome, tipo, pBaseAtaque);
+public class Bubbles extends Pokemon implements Agua {
+
+
+    public Bubbles(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+            int levelProgress, int hp, Ataque atk, int def, int spd, int acuracia) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd, acuracia);
+        //TODO Auto-generated constructor stub
     }
 
-    public int getDano() {
-        return super.getDano();
-    }
-
-    public void setDano(int dano) {
-        super.setDano(dano);
-    }
-
-    public String getNome() {
-        return "Bubbles";
-    }
-
-    public void setNome(String nome) {
-        // Não é necessário definir o nome novamente, já que é fixo como "Bubbles"
-    }
-
-    public String getTipo() {
-        return "Agua";
-    }
-
-    public void setTipo(String tipo) {
-        // Não é necessário definir o tipo novamente, já que é fixo como "Agua"
-    }
-
-    public double getPBaseAtaque() {
-        return super.getpBaseAtaque();
-    }
-
-    public void setPBaseAtaque(double pBaseAtaque) {
-        super.setpBaseAtaque(pBaseAtaque);
+    @Override
+    public String desenhoPokemon() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'desenhoPokemon'");
     }
 }
