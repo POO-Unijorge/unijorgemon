8f34b753d35f5343012dca59f69bce50d4e81aca
Nathan  Michel Silva de Oliveira <08584748598@academico.fja.edu.br>
2023-06-26 20:02:03 -0300

    eevee na main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import org.unijorge.team1.Scyter;
 import org.unijorge.team1.Swarm;
 import org.unijorge.team5.Froakie;
+import org.unijorge.team6.Eevee;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,9 @@ public class Main {
         Swarm atkS = new Swarm(20, "ola", "inseto", 10);
         Scyter p1 = new Scyter(10, 30, 40, null, null, 1, 0, 30, atkS, 40, 60, 10);
         Weedle weedle = new Weedle(10, 30, 40, null, null, 1, 0, 30, atkS, 40, 60, 15, 20); 
-
+        
+        Eevee eevee = new Eevee(5, 20.0, 40.0, null, "Eevee", 1, 0, 55, atkS, 50, 55, 15, 15);
+        
         System.out.println(p1.retornaTipoDoPokemon().get(0).toString());
         System.out.println(p1.getAtk().retornaTipoDoAtaque().get(0).toString());
         ///
