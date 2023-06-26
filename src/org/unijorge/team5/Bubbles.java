fd6a32bdb391ac07355b33ec7a8303cd483180a5
Gabriel Peixoto <67771217+gdspeixoto@users.noreply.github.com>
2023-06-26 19:34:09 -0300

    Correção
    Correção de codigo do pokemon Bubbles

--- src/org/unijorge/team5/Bubbles.java
+++ src/org/unijorge/team5/Bubbles.java
@@ -3,49 +3,39 @@ import org.unijorge.core.Ataque;
 import org.unijorge.core.interfaces.Agua;
 
 public class Bubbles extends Ataque implements Agua {
-    
- public bubbles(){
-  private int dano;
-  private String nome;
-  private String tipo; 
-  private double pBaseAtaque;
-
-  public Bubbles(int dano, String nome, String tipo, double acerto){
-        this.dano = dano;
-        this.nome = nome;
-        this.tipo = tipo;
-        this.pBaseAtaque = pBaseAtaque;
-    }
-    public int getDano() {
-        return dano;
-    }
-    public void setDano(int dano) {
-        this.dano = dano;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = "Bubbles";
-    }
-    public String getTipo() {
-        return tipo;
-    }
-    public void setTipo(String tipo) {
-        this.tipo = "Agua";
-    }
-    public double getPBaseAtaque() {
-        return pBaseAtaque;
-    }
-    public void setPBaseAtaque(double pBaseAtaque) {
-        this.pBaseAtaque = pBaseAtaque;
-    }
-
- }
-  public Bubbles(int dano, String nome, String tipo, double pBaseAtaque) {
+    public Bubbles(int dano, String nome, String tipo, double pBaseAtaque) {
         super(dano, nome, tipo, pBaseAtaque);
-        //TODO Auto-generated constructor stub
     }
 
-    
+    public int getDano() {
+        return super.getDano();
+    }
+
+    public void setDano(int dano) {
+        super.setDano(dano);
+    }
+
+    public String getNome() {
+        return "Bubbles";
+    }
+
+    public void setNome(String nome) {
+        // Não é necessário definir o nome novamente, já que é fixo como "Bubbles"
+    }
+
+    public String getTipo() {
+        return "Agua";
+    }
+
+    public void setTipo(String tipo) {
+        // Não é necessário definir o tipo novamente, já que é fixo como "Agua"
+    }
+
+    public double getPBaseAtaque() {
+        return super.getpBaseAtaque();
+    }
+
+    public void setPBaseAtaque(double pBaseAtaque) {
+        super.setpBaseAtaque(pBaseAtaque);
+    }
 }
