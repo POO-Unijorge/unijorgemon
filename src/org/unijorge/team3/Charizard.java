37144faf0d3a4b97d6a0e8a1a3497f9ba16ddf5d
Ronielly Santos <roniellyicaro33@gmail.com>
2023-06-26 20:53:53 -0300

    adicionado arquivo .java

---
+++ src/org/unijorge/team3/Charizard.java
@@ -0,0 +1,11 @@
+package org.unijorge.team3;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.Animal;
+import org.unijorge.core.Arena;
+
+public class Charizard extends Pokemon{
+
+
+
+}
