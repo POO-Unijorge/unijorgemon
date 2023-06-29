88396012c24c6c469baab5e94d34a5ee32ebe09a
Gustavo Santos <94016306+N11hl@users.noreply.github.com>
2023-06-29 20:56:39 -0300

    Criando TTar

--- src/org/unijorge/team4/Tyranitar.java
+++ src/org/unijorge/team4/Tyranitar.java
@@ -1,1 +1,15 @@
+package org.unijorge.team4;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.enums.Sexo;
+import org.unijorge.core.interfaces.Venenoso;
 
+public class Tyranitar extends Pokemon implements Pedra{
+
+    public Tyranitar(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress,
+                int hp, Ataque atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+        //TODO Auto-generated constructor stub
+        //Modificar
+
+    }
