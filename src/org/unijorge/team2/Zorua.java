9cd8b6d128c53f4d8ff831c53df3a2b9e1911301
Michel <105396013+Meichl@users.noreply.github.com>
2023-06-29 08:34:47 -0300

    Update Zorua.java

--- src/org/unijorge/team2/Zorua.java
+++ src/org/unijorge/team2/Zorua.java
@@ -4,7 +4,7 @@ import org.unijorge.core.Pokemon;
 import org.unijorge.core.enums.Sexo;
 import org.unijorge.core.interfaces.Noturno;
 
-public class Zorua extends Pokemon{
+public class Zorua extends Pokemon implements Noturno{
 
     public Zorua(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, Ataque atk, int def, int spd) {
         super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
