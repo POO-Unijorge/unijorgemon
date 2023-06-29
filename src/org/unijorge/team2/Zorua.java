ed26b011a3f99252bbbe897c60c1eef74c084658
Michel <105396013+Meichl@users.noreply.github.com>
2023-06-29 08:32:30 -0300

    Create Zorua.java

---
+++ src/org/unijorge/team2/Zorua.java
@@ -0,0 +1,18 @@
+package org.unijorge.team7;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.enums.Sexo;
+import org.unijorge.core.interfaces.Noturno;
+
+public class Zorua extends Pokemon{
+
+    public Zorua(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress, int hp, Ataque atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+    }
+
+    public String desenhoPokemon() {
+        return "O_O";
+    }
+
+
+}
