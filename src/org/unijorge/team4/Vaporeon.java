829eb03b49e1e21a06a687f677085a83afbc8f6c
Rodrigo <93962428+s2Alive@users.noreply.github.com>
2023-06-29 20:56:41 -0300

    Update Vaporeon.java

--- src/org/unijorge/team4/Vaporeon.java
+++ src/org/unijorge/team4/Vaporeon.java
@@ -3,11 +3,20 @@ package org.unijorge.team4;
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
 import org.unijorge.core.enums.Sexo;
-import org.unijorge.core.interfaces.Inseto;
+import org.unijorge.core.interfaces.Venenoso;
 
-public class Vaporeon extends Pokemon implements Inseto{
-    public Vaporeon(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
-            int levelProgress, int hp, Ataque atk, int def, int spd, int acuracia, int evasao) {
-        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd, acuracia, evasao);
+public class Vaporeon extends Pokemon implements Venenoso{
+
+    public Vaporeon(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress,
+                int hp, Ataque atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+        //TODO Auto-generated constructor stub
+        //Modificar
+
     }
+    
+        public String desenhoPokemon(){
+        return "(*)_(*)";
+
+    } 
 }
