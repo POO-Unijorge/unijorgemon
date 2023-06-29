b00bf90f4d2ed086b338eb763fe95a12aa0ade92
Rodrigo <93962428+s2Alive@users.noreply.github.com>
2023-06-29 20:54:37 -0300

    Update Vaporeon.java

--- src/org/unijorge/team4/Vaporeon.java
+++ src/org/unijorge/team4/Vaporeon.java
@@ -1,1 +1,13 @@
-Vaporeon
+package org.unijorge.team4;
+
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.enums.Sexo;
+import org.unijorge.core.interfaces.Inseto;
+
+public class Vaporeon extends Pokemon implements Inseto{
+    public Vaporeon(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+            int levelProgress, int hp, Ataque atk, int def, int spd, int acuracia, int evasao) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd, acuracia, evasao);
+    }
+}
