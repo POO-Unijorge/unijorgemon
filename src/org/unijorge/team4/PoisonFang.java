7ff38b9f6e68af618d55d40b3bf6b3b96c54bcc2
Gustavo Santos <94016306+N11hl@users.noreply.github.com>
2023-06-29 21:04:19 -0300

    Atk zubat

---
+++ src/org/unijorge/team4/PoisonFang.java
@@ -0,0 +1,14 @@
+package org.unijorge.team4;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.interfaces.Venenoso;
+
+public class PoisonFang extends Ataque implements Venenoso{
+
+    public PoisonFang(int dano, String nome, String tipo, double pBaseAtaque) {
+        super(dano, nome, tipo, pBaseAtaque);
+        //TODO Auto-generated constructor stub
+    }
+
+
+
+}
