55f157df6ac1317a82b6f1c44eaab8829563a6cf
Ricardo <94655851+Carto100@users.noreply.github.com>
2023-06-29 21:20:23 -0300

    Update Pikachu.java

--- src/org/unijorge/team6/Pikachu.java
+++ src/org/unijorge/team6/Pikachu.java
@@ -1,1 +1,17 @@
+import org.unijorge.core.Ataque;
+import org.unijorge.core.Pokemon;
+import org.unijorge.core.enums.Sexo;
+import org.unijorge.core.interfaces.Normal;
 
+public class Pikachu extends Pokemon implements Elétrico{
+
+    public Lopunny(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+            int levelProgress, int hp, Ataque atk, int def, int spd) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+        //TODO Auto-generated constructor stub
+
+    }
+        public String desenhoPokemon(){
+        return "(*)_(*)";
+    } 
+}
