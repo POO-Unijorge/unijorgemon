e1d6038cfc3659b5797815e09377d3ebc3b29597
Rodrigo <93962428+s2Alive@users.noreply.github.com>
2023-06-29 21:09:34 -0300

    Create HydroPump.java

---
+++ src/org/unijorge/team4/HydroPump.java
@@ -0,0 +1,13 @@
+package org.unijorge.team4;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.interfaces.Agua;
+
+public class HydroPump extends Ataque implements Agua{
+
+    public PoisonFang(int dano, String nome, String tipo, double pBaseAtaque) {
+        super(dano, nome, tipo, pBaseAtaque);
+        //TODO Auto-generated constructor stub
+    }
+
+
+}
