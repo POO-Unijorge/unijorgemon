96ed7b617a6d371bde73747293da4b12405bea8d
Bruno Durão Silva <80107106+zeimi@users.noreply.github.com>
2023-06-29 21:27:18 -0300

    Create MegaPunch.java

---
+++ src/org/unijorge/team4/MegaPunch.java
@@ -0,0 +1,10 @@
+package org.unijorge.team4;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.interfaces.Lutador;
+
+public class MegaPunch extends Ataque implements Lutador{
+
+    public MegaPunch(int dano, String nome, String tipo, double pBaseAtaque) {
+        super(dano, nome, tipo, pBaseAtaque);
+    }
+
