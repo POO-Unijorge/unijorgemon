79ec6fe345f20d2deeece165e958648f433c93ec
Ricardo <94655851+Carto100@users.noreply.github.com>
2023-06-29 21:34:29 -0300

    Update Thunderbolt.java

--- src/org/unijorge/team6/Thunderbolt.java
+++ src/org/unijorge/team6/Thunderbolt.java
@@ -1,1 +1,28 @@
+class Pikachu {
+    private int hp;
+    
+    public Pikachu(int initialHp) {
+        this.hp = initialHp;
+    }
+    
+    public void thunderboltAttack() {
+        int damage = 20; // Quantidade de dano causado pelo Thunderbolt
+        hp -= damage;
+        System.out.println("Thunderbolt atingiu o oponente! HP restante: " + hp);
+    }
+    
+    public int getHp() {
+        return hp;
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+        Pikachu pikachu = new Pikachu(100); // Cria um Pikachu com 100 de HP
+        
+        pikachu.thunderboltAttack(); // Executa o ataque Thunderbolt
+        
+        int remainingHp = pikachu.getHp(); // Obtém o HP restante do Pikachu
+        System.out.println("HP restante do Pikachu: " + remainingHp);
+    }
+}
