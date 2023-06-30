d931299d488f11631216e84e469e8301a919f1bd
jairolu565 <105252484+jairolu565@users.noreply.github.com>
2023-06-29 21:27:39 -0300

    Update SolarPower.java

--- src/org/unijorge/team6/SolarPower.java
+++ src/org/unijorge/team6/SolarPower.java
@@ -9,32 +9,8 @@ public class SolarPower extends Ataque implements Normal{
          // Get e Set nos atributos
             public void set Hp(int hp){
                 if (getHp < 33){
-                    this.setHP(getHP()+1);
-                    
-                } else {
-                    this.setHp(this.getHp()-hp); // Colocando o dano
-                }
-                if (getHp() <= 0){}
-                else if (getHp() > 50){
-                    SuperLopnnuy superLopunny = new SuperLopnnuy();
-                    // Batalhar com o personagem em espefico ela vai virar supe lopunny
-                    // colocar o super atk
-                    superLopunny.setHp(getHp());
-                    superLopunny.setAtk(getAtk());
-                    superLopunny.setAtkName("Super JumpKick");
-                    // get do super
-                    this.hp = superLopunny.getHp();
-                    this.atk = superLopunny.getAtk();
-                }
-
-            public int getLevelProgress(){
-                return levelProgress;
-            }
-
-            public void setLevelProgress(int levelProgress){
-                this.levelProgress = levelProgress;
-
-            }
+                    this.setHP(getHP()+1);   
+                } 
 
           }
         }
