5c0221396f5eb5d7a2a0ea86102a798b36d6f239
Rodrigo <93962428+s2Alive@users.noreply.github.com>
2023-06-29 21:01:21 -0300

    Update Vaporeon.java

--- src/org/unijorge/team4/Vaporeon.java
+++ src/org/unijorge/team4/Vaporeon.java
@@ -3,9 +3,9 @@ package org.unijorge.team4;
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
 import org.unijorge.core.enums.Sexo;
-import org.unijorge.core.interfaces.Venenoso;
+import org.unijorge.core.interfaces.Agua;
 
-public class Vaporeon extends Pokemon implements Venenoso{
+public class Vaporeon extends Pokemon implements Agua{
 
     public Vaporeon(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level, int levelProgress,
                 int hp, Ataque atk, int def, int spd) {
