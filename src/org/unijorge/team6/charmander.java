0b4bf20d4f26feb68c6049242bafa31db1785a18
jairolu565 <105252484+jairolu565@users.noreply.github.com>
2023-06-29 21:19:00 -0300

    Update charmander.java

--- src/org/unijorge/team6/charmander.java
+++ src/org/unijorge/team6/charmander.java
@@ -3,7 +3,7 @@ import org.unijorge.core.Pokemon;
 import org.unijorge.core.enums.Sexo;
 import org.unijorge.core.interfaces.Normal;
 
-public class Lopunny extends Pokemon implements Normal{
+public class Charmander extends Pokemon implements Normal{
 
     public Lopunny(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
             int levelProgress, int hp, Ataque atk, int def, int spd) {
