5d9de2b7ac06e227c9e897af34e462a3ab104fa0
Rodrigo <93962428+s2Alive@users.noreply.github.com>
2023-06-29 21:18:22 -0300

    Update HydroPump.java

--- src/org/unijorge/team4/HydroPump.java
+++ src/org/unijorge/team4/HydroPump.java
@@ -6,7 +6,6 @@ public class HydroPump extends Ataque implements Agua{
 
     public HydroPump(int dano, String nome, String tipo, double pBaseAtaque) {
         super(dano, nome, tipo, pBaseAtaque);
-        //TODO Auto-generated constructor stub
     }
 
 
