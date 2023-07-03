60f2eb9122b209be21c71bdf455815a894b13701
Hauntedghostjr <129462880+Hauntedghostjr@users.noreply.github.com>
2023-07-03 20:48:48 -0300

    Tentando resolver os erros

--- src/org/unijorge/team6/Charmander.java
+++ src/org/unijorge/team6/Charmander.java
@@ -5,7 +5,7 @@ import org.unijorge.core.interfaces.Normal;
 
 public class Charmander extends Pokemon implements Fogo{
 
-    public Lopunny(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+    public Charmander(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
             int levelProgress, int hp, Ataque atk, int def, int spd) {
         super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
         //TODO Auto-generated constructor stub
