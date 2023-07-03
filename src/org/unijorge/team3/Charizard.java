f265a9f55af638835e06ebcf214b3ebda8b25b88
RoniSuper <roniellyicaro33@gmail.com>
2023-07-03 20:47:52 -0300

    team3

--- src/org/unijorge/team3/Charizard.java
+++ src/org/unijorge/team3/Charizard.java
@@ -1,10 +1,11 @@
 package org.unijorge.team3;
+
 import org.unijorge.core.Ataque;
 import org.unijorge.core.Pokemon;
 import org.unijorge.core.enums.Sexo;
 import org.unijorge.core.interfaces.Fogo;
 
-public class Charizard extends Pokemon{
+public class Charizard extends Pokemon implements Fogo{
 
 	public Charizard (int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
 			int levelProgress, int hp, Ataque atk, int def, int spd) {
