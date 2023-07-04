a9998bab81763254e0617877a2a9b2e585c6b0e5
mxrlla <marllasousar@gmail.com>
2023-07-03 21:02:30 -0300

    Update.Lopunny

--- src/org/unijorge/team6/JumpKick.java
+++ src/org/unijorge/team6/JumpKick.java
@@ -1,4 +1,3 @@
-import org.unijorge.core.team6;
 import org.unijorge.core.Ataque;
 import org.unijorge.core.interfaces.Normal;
 
@@ -7,35 +6,6 @@ public class JumpKick extends Ataque implements Normal{
         super(dano, nome, tipo, pBaseAtaque);
         }
          // Get e Set nos atributos
-            public void set Hp(int hp){
-                if (getHp < 50){
-                    this.setHP(getHP()+1);
-                    // Verificando se a vida da coelhinha da menor que 50, estando vai acresentar 1 ajudinha.
-                } else {
-                    this.setHp(this.getHp()-hp); // Colocando o dano
-                }
-                if (getHp() <= 0){}
-                else if (getHp() > 50){
-                    SuperLopnnuy superLopunny = new SuperLopnnuy();
-                    // Batalhar com o personagem em espefico ela vai virar supe lopunny
-                    // colocar o super atk
-                    superLopunny.setHp(getHp());
-                    superLopunny.setAtk(getAtk());
-                    superLopunny.setAtkName("Super JumpKick");
-                    // get do super
-                    this.hp = superLopunny.getHp();
-                    this.atk = superLopunny.getAtk();
-                }
+}
 
-            public int getLevelProgress(){
-                return levelProgress;
-            }
-
-            public void setLevelProgress(int levelProgress){
-                this.levelProgress = levelProgress;
-
-            }
-
-          }
-        }
      
--- src/org/unijorge/team6/Lopunny.java
+++ src/org/unijorge/team6/Lopunny.java
@@ -5,13 +5,51 @@ import org.unijorge.core.interfaces.Normal;
 
 public class Lopunny extends Pokemon implements Normal{
 
-    public Lopunny(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
-            int levelProgress, int hp, Ataque atk, int def, int spd) {
-        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd);
+        public Lopunny(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+            int levelProgress, int hp, Ataque atk, int def, int spd, int acuracia, int evasao) {
+        super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd, acuracia, evasao);
         //TODO Auto-generated constructor stub
-
     }
+
         public String desenhoPokemon(){
         return "(*)_(*)";
     } 
+
+
+    class SuperLopnnuy extends Lopunny{
+
+        public SuperLopnnuy(int idade, double peso, double tamanho, Sexo sexo, String nomeCientifico, int level,
+                int levelProgress, int hp, Ataque atk, int def, int spd, int acuracia, int evasao) {
+            super(idade, peso, tamanho, sexo, nomeCientifico, level, levelProgress, hp, atk, def, spd, acuracia, evasao);
+            //TODO Auto-generated constructor stub
+        }
+        
+    }
+
+    public void setHp(int hp){
+        if (hp < 50){
+            this.setHp(getHp()+1);
+            // Verificando se a vida da coelhinha da menor que 50, estando vai acresentar 1 ajudinha.
+        } else {
+            this.setHp(this.getHp()-hp); // Colocando o dano
+        }
+        if (getHp() <= 0){}
+        else if (getHp() > 50){
+         
+            SuperLopnnuy superLopunny = new SuperLopnnuy(this.getIdade(), this.getPeso(), this.getTamanho(),  
+            this.getSexo(), this.getNomeCientifico(), this.getLevel(), this.getLevelProgress(), this.getHp()+30,
+             this.getAtk(), this.getDef(), this.getSpd(), this.getAcuracia(),getEvasao()) ;
+             // Criou a classe do pok evoluido
+            // Batalhar com o personagem em espefico ela vai virar super lopunny
+            // colocar o super atk
+            superLopunny.setHp(getHp());
+            superLopunny.setAtk(getAtk());
+            superLopunny.getAtk().setNome("Super JumpKick");
+            // get do super
+            setHp(superLopunny.getHp());
+            setAtk(superLopunny.getAtk());
+        }
+
+        
+    }
 }
