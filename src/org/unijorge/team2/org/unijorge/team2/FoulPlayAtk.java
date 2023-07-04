954697b23ba76232912d960e8368bf186cc1fa04
Michel <105396013+Meichl@users.noreply.github.com>
2023-07-04 10:51:50 -0300

    Update FoulPlayAtk.java

--- src/org/unijorge/team2/org/unijorge/team2/FoulPlayAtk.java
+++ src/org/unijorge/team2/org/unijorge/team2/FoulPlayAtk.java
@@ -1,1 +1,14 @@
+package org.unijorge.team2;
+import org.unijorge.core.Ataque;
+import org.unijorge.core.interfaces.Dark;
 
+public class FoulPlay extends Ataque implements Dark{
+
+    public FoulPlay(int dano, String nome, String tipo, double pBaseAtaque) {
+        super(dano, nome, tipo, pBaseAtaque);
+        //TODO Auto-generated constructor stub
+    }
+
+
+
+}
